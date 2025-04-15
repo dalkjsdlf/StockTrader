bc43a1b2877cb40e97914fb323b1f4704d0dfed3
dorris <dalkjsdlf@gmail.com>
2025-04-15 21:44:41 +0900

    refactor: OrderService 인터페이스화
    OrderService를 인터페이스로 만들고 OrderServiceImpl을 구현체로 만듦

--- src/main/java/io/ratel/stocktrader/business/order/api/controller/OrderController.java
+++ src/main/java/io/ratel/stocktrader/business/order/api/controller/OrderController.java
@@ -3,7 +3,7 @@ package io.ratel.stocktrader.business.order.api.controller;
 import io.ratel.stocktrader.business.order.api.request.BuyOrderRequest;
 import io.ratel.stocktrader.business.order.api.response.BuyOrderResponse;
 import io.ratel.stocktrader.business.order.domain.entity.Order;
-import io.ratel.stocktrader.business.order.domain.service.OrderService;
+import io.ratel.stocktrader.business.order.domain.service.OrderServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,18 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/order")
 @RequiredArgsConstructor
 public class OrderController {
-    private final OrderService orderService;
+    private final OrderServiceImpl orderService;
 
     /**
      * 주식 매수 주문
-     * @param BuyOrderRequest 주문 요청 DTO
+     * @param buyOrderRequest 주문 요청 DTO
      * @return 주문 결과 응답 DTO
      */
     @PostMapping("/buy")
     public ResponseEntity<BuyOrderResponse> placeBuyOrder(@Valid @RequestBody BuyOrderRequest buyOrderRequest) {
-        Order order = buyOrderRequest.toDomain(); // DTO가 Domain으로 변환
-        Order savedOrder = orderService.buyStock(order);
-        BuyOrderResponse response = BuyOrderResponse.from(savedOrder); // Domain을 DTO로 변환
+        final Order order = buyOrderRequest.toDomain(); // DTO가 Domain으로 변환
+        final Order savedOrder = orderService.buyStock(order);
+        final BuyOrderResponse response = BuyOrderResponse.from(savedOrder); // Domain을 DTO로 변환
         return ResponseEntity.status(201).body(response);
     }
 }
