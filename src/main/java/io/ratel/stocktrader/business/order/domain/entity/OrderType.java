f5a357d380a4ecfb8ca01a2825f32c6fab28aa2e
dorris <dalkjsdlf@gmail.com>
2025-04-15 21:45:32 +0900

    chore: 불필요한 주석 제거
    class별로 자동 추가된 파일의 기본정보가 기록된 주석을 제거함

--- src/main/java/io/ratel/stocktrader/business/order/domain/entity/OrderType.java
+++ src/main/java/io/ratel/stocktrader/business/order/domain/entity/OrderType.java
@@ -1,16 +1,5 @@
 package io.ratel.stocktrader.business.order.domain.entity;
 
-/**
- * packageName  : io.ratel.stocktrader.business.order.domain.entity
- * fileName    : OrderType
- * author      : dorris
- * date        : 2025. 4. 12.
- * description :
- * ================================================
- * DATE              AUTHOR              NOTE
- * ================================================
- * 2025. 4. 12.          dorris             최초생성
- */
 public enum OrderType {
     LIMIT,    // 지정가 주문
     MARKET    // 시장가 주문
